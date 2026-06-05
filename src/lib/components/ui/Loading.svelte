b0ed46c5d6da862543be5ed5e903f6302353e978
Theo Kapi <theo_kapi@outlook.com>
2026-06-04 20:21:32 -0700

    Issues #12 & #35

--- src/lib/components/ui/Loading.svelte
+++ src/lib/components/ui/Loading.svelte
@@ -5,7 +5,7 @@
 </script>
 
 {#if show}
-	<div class="fixed inset-0 z-50 flex cursor-not-allowed items-center justify-center bg-(--bg)">
+	<div class="fixed inset-0 z-50 flex cursor-not-allowed items-center justify-center bg-(--bg) overflow-hidden">
 		<Loader class="animate-spin text-(--p)" size={40} />
 	</div>
 {/if}
