9ee359b478c7a9d0cbd088812167be740aa19046
Theo Kapi <theo_kapi@outlook.com>
2026-06-05 17:50:11 -0700

    Cleaning up code

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,14 +2,6 @@ import { writable } from 'svelte/store';
 
 export const changed = writable(false);
 
-export type Character = {
-	value: string;
-	bold: boolean;
-	italic: boolean;
-	underline: boolean;
-	fontSize: number;
-};
-
 export type DocumentLink = {
 	title: string;
 	code: string;
