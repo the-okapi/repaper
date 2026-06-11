a97ef7abdd0ae6547be9f55a3e420458361fe1df
Theo Kapi <theo_kapi@outlook.com>
2026-06-10 19:31:29 -0700

    Issue #64

--- src/lib/components/ui/Slider.svelte
+++ src/lib/components/ui/Slider.svelte
@@ -10,6 +10,6 @@
 	</span>
 	<Slider.Thumb
 		index={0}
-		class="flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-(--p) focus:outline-none"
+		class="flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-(--fg) focus:outline-none"
 	/>
 </Slider.Root>
