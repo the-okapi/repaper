4598c33e9aea64e5cf6ec3b0da3d766216687e28
Theo Kapi <theo_kapi@outlook.com>
2026-06-13 08:24:17 -0700

    Issue #69

--- src/lib/pdf.ts
+++ src/lib/pdf.ts
@@ -6,16 +6,22 @@ export async function downloadDocument(text: string) {
 	const html2pdf = (await import('html2pdf.js')).default;
 
 	const container = document.createElement('div');
+	container.id = 'pdf';
 	container.innerHTML = text;
 
 	container.style.color = '#000';
 	container.style.backgroundColor = '#fff';
+	container.style.paddingBottom = '10px';
 
 	html2pdf()
 		.from(container)
 		.set({
 			margin: 10,
-			filename: 'Document.pdf'
+			filename: 'Document.pdf',
+			// @ts-expect-error For some reason doesn't detect this property
+			pagebreak: {
+				mode: 'avoid-all'
+			}
 		})
 		.save();
 }
