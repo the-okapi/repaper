1598fff8da906e71f6098a73ff4efd28db1e4cff
the-okapi <156860750+the-okapi@users.noreply.github.com>
2026-09-01 21:01:08 -0700

    Updating

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,29 +2,36 @@
 	<title>Repaper</title>
 </svelte:head>
 
-<div class="relative flex h-[calc(100vh-5rem)] w-screen items-stretch justify-center px-10 py-7">
-	<div class="flex h-full w-full flex-col items-center justify-center overflow-hidden">
-		<h1>AI BE</h1>
-		<h1 class="gone">GONE</h1>
+<div class="relative flex h-[calc(100vh-5rem)] w-screen items-stretch justify-center px-7">
+	<div class="ml-5 flex h-full w-full flex-col justify-center overflow-hidden">
+		<h1 class="gradient gradient1">Real</h1>
+		<h1>People</h1>
+		<div class="h-15"></div>
+		<h1 class="gradient gradient2">Real</h1>
+		<h1>Writing</h1>
 	</div>
 	<div class="w-20"></div>
-	<div class="h-full w-200 rounded-3xl border border-(--o) bg-(--bg) p-7">
-		<h2 class="mb-3 text-center text-5xl font-bold">Repaper</h2>
-		<p class="text-center text-2xl">Real Writing, Real People</p>
-		<div class="h-5"></div>
-		<ul class="text-xl">
-			<li>No Copy/Paste from AI</li>
-			<li>Assignments</li>
-		</ul>
+	<div class="flex w-200 flex-col">
+		<div class="h-7"></div>
+		<div class="h-full w-180 rounded-3xl border border-(--o) bg-(--bg) p-7">
+			<h2 class="gradient gradient2 gradient3 mb-3 text-center text-5xl font-bold">
+				Repaper
+			</h2>
+			<div class="h-5"></div>
+			<ul class="text-xl">
+				<li>No Copy/Paste from AI</li>
+				<li>Assignments</li>
+			</ul>
+		</div>
+		<div class="h-7"></div>
 	</div>
 </div>
 
 <style>
 	h1 {
-		font-size: 16rem;
+		font-size: 9rem;
 		font-weight: 900;
-		line-height: 14rem;
-		letter-spacing: -0.5rem;
+		line-height: 8rem;
 	}
 
 	@keyframes gradient {
@@ -39,23 +46,39 @@
 		}
 	}
 
-	.gone {
+	.gradient1 {
 		background: linear-gradient(
 			90deg,
-			var(--red),
 			var(--red),
 			var(--yellow),
 			var(--green),
 			var(--turquoise),
 			var(--blue),
-			var(--purple),
 			var(--purple)
 		);
+	}
+
+	.gradient2 {
+		background: linear-gradient(
+			90deg,
+			var(--purple),
+			var(--blue),
+			var(--turquoise),
+			var(--green),
+			var(--yellow),
+			var(--red)
+		);
+	}
+
+	.gradient {
 		background-size: 400% 100%;
-		width: fit-content;
 		background-clip: text;
 		color: transparent;
-		animation: gradient 30s linear infinite;
+		animation: gradient 50s linear infinite;
+	}
+
+	.gradient3 {
+		animation: gradient 30s linear infinite !important;
 	}
 
 	li {
